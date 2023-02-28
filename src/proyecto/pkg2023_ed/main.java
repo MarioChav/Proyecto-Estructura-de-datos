bfe7f8dc29622e1b9bcb0f5572f4f9c38b3e979d
mario <mario@DESKTOP-79MCB0S>
2023-02-27 23:08:22 -0600

    Base del proyecto-pruebas2

--- src/proyecto/pkg2023_ed/main.java
+++ src/proyecto/pkg2023_ed/main.java
@@ -4,9 +4,14 @@ package proyecto.pkg2023_ed;
 
 public class main {
 
+    
+    
  
     public static void main(String[] args) {
         // TODO code application logic here
+        // TODO code application logic here
+        // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
