480a11190d206e7971dcdbc38dcf7ef1cb3f2049
FerUwu <114791752+FerUwu19@users.noreply.github.com>
2023-02-27 23:41:04 -0600

    commit Prueba

--- src/proyecto/pkg2023_ed/main.java
+++ src/proyecto/pkg2023_ed/main.java
@@ -12,8 +12,17 @@ public class main {
         // TODO code application logic here
         // TODO code application logic here
         System.out.println("Hola");
+<<<<<<< HEAD
+        Systemas
+>>>>>>> a65bed06cd01286dd719b64328b1699d417789d4
+=======
         System.out.println("version prueba");
+
+        System.out.println("Prueba fer");
         
+>>>>>>> 60b4032c7f0a52c059da6fc09c09e72554eece6f
+
+
     }
     
 }
