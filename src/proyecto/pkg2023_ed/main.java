39436e4199181a395bb91213ea4ac85e666a365b
FerUwu19 <114791752+FerUwu19@users.noreply.github.com>
2023-03-02 00:39:56 -0600

    Interfaces

--- src/proyecto/pkg2023_ed/main.java
+++ src/proyecto/pkg2023_ed/main.java
@@ -5,7 +5,13 @@ package proyecto.pkg2023_ed;
 public class main {
  
     public static void main(String[] args) {
-       
+       Inicio();
     }
     
+    public static void Inicio(){
+        Ingresar inicio = new Ingresar();
+        inicio.setTitle("Ingreso del juego");
+        inicio.setLocationRelativeTo(null);
+        inicio.setVisible(true);
+    }
 }
