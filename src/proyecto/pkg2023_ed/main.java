a5f8305ecb20a7f0471cc1c82ea97be511837f4f
FerUwu <114791752+FerUwu19@users.noreply.github.com>
2023-03-01 23:36:19 -0600

    Clean Main

--- src/proyecto/pkg2023_ed/main.java
+++ src/proyecto/pkg2023_ed/main.java
@@ -3,29 +3,9 @@ package proyecto.pkg2023_ed;
 
 
 public class main {
-
-    
-    
  
     public static void main(String[] args) {
-        // TODO code application logic here
-        // TODO code application logic here
-        // TODO code application logic here
-        System.out.println("Hola");
-
-        System.out.println("version prueba");
-
-        System.out.println("Prueba fer");
-        
-        System.out.println("Otra prueba");
-
-        System.out.println("v5");
-
-        System.out.println("prueba ");
-        
-        System.out.println("jajajajaja");
-        
-        System.out.println("lol");
+       
     }
     
 }
