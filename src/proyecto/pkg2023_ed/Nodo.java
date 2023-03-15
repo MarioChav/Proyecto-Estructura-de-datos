7776dea14b634e2a78c15737c1aaae103040f7c0
PlumbBeast <davi-d57@hotmail.com>
2023-03-14 23:13:45 -0600

    Update Nodo.java

--- src/proyecto/pkg2023_ed/Nodo.java
+++ src/proyecto/pkg2023_ed/Nodo.java
@@ -6,5 +6,9 @@ package proyecto.pkg2023_ed;
  * @author Joel Lopez
  */
 public class Nodo {
+    private CarroCarga carCarga;
+    private MaquinariaPesada maqPesada;
+    private SuperAuto sAuto;
+    private Nodo next;
     
 }
