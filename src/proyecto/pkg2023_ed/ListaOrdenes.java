6a12d515fb552e1ae6d9440cc7a6fa4425c560f4
PlumbBeast <davi-d57@hotmail.com>
2023-03-14 23:32:12 -0600

    Update ListaOrdenes.java

--- src/proyecto/pkg2023_ed/ListaOrdenes.java
+++ src/proyecto/pkg2023_ed/ListaOrdenes.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyecto.pkg2023_ed;
 
 /**
@@ -9,5 +5,23 @@ package proyecto.pkg2023_ed;
  * @author Joel Lopez
  */
 public class ListaOrdenes {
-    
+
+    private Nodo cabeza;
+
+    public void inserta(int tipo) {
+        if (cabeza == null) { //caso 1    
+            switch (tipo) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+//            cabeza = new Nodo(tipo);
+        } else if (cabeza.getNext() == null) { //caso 3            
+//            cabeza.setNext(new Nodo(tipo));
+        }
+    }
 }
